3aeac1dae9d77d0626e79ebbac9000071e3658ff
Guilherme Calça <guilhermecalca@hotmail.com>
2023-06-21 22:36:11 -0300

    Add Firebase

--- app/src/main/java/com/example/adoptionapp/ui/login/LoginFragment.kt
+++ app/src/main/java/com/example/adoptionapp/ui/login/LoginFragment.kt
@@ -1,11 +1,15 @@
 package com.example.adoptionapp.ui.login
 
 import android.os.Bundle
+import android.text.TextUtils
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.appcompat.content.res.AppCompatResources
 import com.example.adoptionapp.FragmentNavigation
 import com.example.adoptionapp.R
 import com.example.adoptionapp.ui.register.RegisterFragment
@@ -17,15 +21,13 @@ private const val ARG_PARAM2 = "param2"
 
 
 class LoginFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    private lateinit var username: EditText
+    private lateinit var password: EditText
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
+
         }
     }
 
@@ -36,23 +38,47 @@ class LoginFragment : Fragment() {
         // Inflate the layout for this fragment
         var view = inflater.inflate(R.layout.fragment_login, container, false)
 
+        username = view.findViewById(R.id.log_username)
+        password = view.findViewById(R.id.log_password)
+
         view.findViewById<Button>(R.id.btn_register).setOnClickListener {
             var navRegister = activity as FragmentNavigation
             navRegister.navigateFrag(RegisterFragment(), false)
         }
+
+        view.findViewById<Button>(R.id.btn_login).setOnClickListener {
+            validateForm()
+        }
+
         return view
     }
 
+    private fun validateForm() {
+        val icon = AppCompatResources.getDrawable(requireContext(),
+            R.drawable.baseline_warning_24)
+
+        icon?.setBounds(0, 0, icon.intrinsicWidth, icon.intrinsicHeight)
+        when {
+            TextUtils.isEmpty(username.text.toString().trim())->{
+                username.setError("Insira um Usuário", icon)
+            }
+            TextUtils.isEmpty(password.text.toString().trim())->{
+                password.setError("Insira uma Senha", icon)
+            }
+
+            username.text.toString().isNotEmpty() &&
+                    password.text.toString().isNotEmpty() ->
+            {
+                if (username.text.toString().matches(Regex("[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+"))) {
+                    Toast.makeText(context, "Login Concluído",  Toast.LENGTH_SHORT).show()
+                } else {
+                    username.setError("Insira um Nome Válido", icon)
+                }
+            }
+        }
+    }
+
     companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment LoginFragment.
-         */
-        // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
             LoginFragment().apply {
--- app/src/main/java/com/example/adoptionapp/ui/register/RegisterFragment.kt
+++ app/src/main/java/com/example/adoptionapp/ui/register/RegisterFragment.kt
@@ -8,9 +8,12 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
+import androidx.appcompat.content.res.AppCompatResources
 import com.example.adoptionapp.FragmentNavigation
 import com.example.adoptionapp.R
 import com.example.adoptionapp.ui.login.LoginFragment
+import com.google.firebase.auth.FirebaseAuth
 
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 private const val ARG_PARAM1 = "param1"
@@ -20,14 +23,13 @@ private const val ARG_PARAM2 = "param2"
 class RegisterFragment : Fragment() {
     private lateinit var username: EditText
     private lateinit var password: EditText
-    private lateinit var confirm_password: EditText
-
+    private lateinit var confirmPassword: EditText
+    private lateinit var fAuth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
-            username = it.getString(ARG_PARAM1)
-            password= it.getString(ARG_PARAM2)
+
         }
     }
 
@@ -40,7 +42,7 @@ class RegisterFragment : Fragment() {
 
         username = view.findViewById(R.id.reg_username)
         password = view.findViewById(R.id.reg_password)
-        confirm_password = view.findViewById(R.id.reg_confirm_password)
+        confirmPassword = view.findViewById(R.id.reg_confirm_password)
 
         view.findViewById<Button>(R.id.btn_login_reg).setOnClickListener {
             var navRegister = activity as FragmentNavigation
@@ -54,9 +56,39 @@ class RegisterFragment : Fragment() {
     }
 
     private  fun validateEmptyForm(){
+        val icon = AppCompatResources.getDrawable(requireContext(),
+        R.drawable.baseline_warning_24)
+
+        icon?.setBounds(0, 0, icon.intrinsicWidth, icon.intrinsicHeight)
        when {
            TextUtils.isEmpty(username.text.toString().trim())->{
-               username.setError("Insira um Usuário")
+               username.setError("Insira um Usuário", icon)
+           }
+           TextUtils.isEmpty(password.text.toString().trim())->{
+               password.setError("Insira uma Senha", icon)
+           }
+           TextUtils.isEmpty(confirmPassword.text.toString().trim())->{
+               confirmPassword.setError("Insira a Senha Novamente", icon)
+           }
+
+           username.text.toString().isNotEmpty() &&
+                   password.text.toString().isNotEmpty() &&
+                        confirmPassword.text.toString().isNotEmpty() ->
+           {
+               if (username.text.toString().matches(Regex("[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+"))) {
+                    if (password.text.toString().length >= 8) {
+                        if (confirmPassword.text.toString() == password.text.toString()) {
+                            Toast.makeText(context, "Registro Concluído", Toast.LENGTH_SHORT).show()
+                        } else {
+                            confirmPassword.setError("As senhas Não Coincidem", icon)
+                        }
+
+                    } else {
+                        password.setError("A Senha Deve Conter ao Menos 8 Caracteres", icon)
+                    }
+               } else {
+                   username.setError("Insira um Nome Válido", icon)
+               }
            }
        }
     }
